7111bf646cdf752017748279b12eda452826b24a
CristiAllan <cristiallan@gmail.com>
2018-05-22 10:15:33 -0300

    include highlightable to movie and serie

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,5 +1,6 @@
 class Serie < ApplicationRecord
-  include​ PgSearch
+  include PgSearch
+  include Highlightable
   multisearchable against: [:title]
   belongs_to :category
   has_many :reviews, as: :reviewable
