03c283bf7d92a3bd15ffdddc272138438cdec55a
CristiAllan <cristiallan@gmail.com>
2018-05-22 10:09:31 -0300

    develop models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :reviews
+  has_many :favorites
+  has_many :players
+  validates :name, presence: true, on: :update
 end
 
